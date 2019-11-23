594917034dd44da2d304c2a09803487a21fd8996
Titan Lien <titan.lien@gmail.com>
2019-11-23 23:27:38 +0100

    wilcard the ami image name

--- task1/worker/main.tf
+++ task1/worker/main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "work" {
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["ubuntu-bionic-18.04-amd64-server-20180522-dotnetcore-2018.07.11"]
+    values = ["ubuntu-bionic-18.04-amd64-server-*"]
   }
 
 }
@@ -12,7 +12,7 @@ data "aws_availability_zones" "this" {}
 
 resource "aws_spot_instance_request" "worker" {
   count                  = var.number_instances
-  availability_zone      = data.aws_availability_zones.this.names[count.index]
+  availability_zone      = data.aws_availability_zones.this.names[count.index % 2]
   ami                    = data.aws_ami.work.id
   instance_type          = "t3a.micro"
   key_name               = var.key_name
@@ -20,7 +20,11 @@ resource "aws_spot_instance_request" "worker" {
   spot_price             = "0.01"
   spot_type              = "one-time"
   wait_for_fulfillment   = true
-  subnet_id              = var.vpc.public_subnets[count.index]
+  subnet_id              = var.vpc.public_subnets[count.index % 2]
+
+  tags = {
+    index = count.index
+  }
 
   # force Terraform to wait until a connection can be made, so that Ansible doesn't fail when trying to provision
   provisioner "remote-exec" {
@@ -31,7 +35,7 @@ resource "aws_spot_instance_request" "worker" {
       user        = "ubuntu"
       type        = "ssh"
       private_key = file(var.pvt_key)
-      host        = aws_spot_instance_request.worker[count.index].public_ip
+      host        = self.public_ip
     }
   }
 }
@@ -64,7 +68,7 @@ resource "null_resource" "cmd" {
 resource "null_resource" "ansible-worker" {
   count     = var.number_instances
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${aws_spot_instance_request.worker[count.index].public_ip},' ./ansible/site.yaml -u ubuntu"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${aws_spot_instance_request.worker[*].public_ip},' ./ansible/site.yaml -u ubuntu"
   }
 
   depends_on = [null_resource.cmd]
