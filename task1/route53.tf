9eaae72ea039338e082def96fd2953412233855d
MaoHsiang Lien <admin@mhlien.tw>
2020-02-01 16:53:37 +0100

    add route53 subdomain

---
+++ task1/route53.tf
@@ -0,0 +1,11 @@
+data "aws_route53_zone" "mhlien" {
+  name = "mhlien.de."
+}
+
+resource "aws_route53_record" "task1" {
+  zone_id = data.aws_route53_zone.mhlien.zone_id
+  name    = "task1"
+  type    = "CNAME"
+  ttl     = "30"
+  records = [ aws_elb.task1.dns_name ]
+}
