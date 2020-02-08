afc710f221d4b1426823fce8e6805fad4f31c70f
MaoHsiang Lien <admin@mhlien.tw>
2020-02-08 22:23:40 +0100

    Using kustomize instead of create template file

--- task3/deploy_webapp.sh
+++ task3/deploy_webapp.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -e
 
-export webapp_tag=`git rev-parse --verify HEAD`
-
-# update Menifests 
-yasha --webapp_tag=$webapp_tag -v variables/common.yml -o web_1.yml --keep-trailing-newline \
-  templates/web_1.yml.j2
-exit 0
-
-yasha -v variables/common.yml -o web_2.yml --keep-trailing-newline \
-  templates/web_2.yml.j2
-
-# kubectl appfy
-kubectl apply -f web_1.yml -f web_2.yml
+# kustomize + kubectl appfy
+kustomize build base/ | kubectl apply -f -
 
 # wait pods is ready
 sleep 5
